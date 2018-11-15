71bdc8b1b3cfc604cecc7e2ba855cca30323406b
pkkan <pk.kandru@gmail.com>
2018-11-15 00:18:48 -0500

    Getting correct information for classes API, parsing required

--- ufc_api/routes/api/classes.js
+++ ufc_api/routes/api/classes.js
@@ -12,11 +12,11 @@ router.get('/', (req, res) => {
     request('https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23', function (error, response, body) {
         if((response && response.statusCode) === 200) {
             const $ = cheerio.load(body);
-            const list = $('#block-mainpagecontent div:nth-child(1) div.block-facet--links:nth-child(2) div.facets-widget-links ul li.facet-item').text();
-            const arr = list.trimRight().split('\n');
+            const weightclasses = $('#block-mainpagecontent div:nth-child(1) div.block-facet--links:nth-child(2) div.facets-widget-links ul li.facet-item').text();
+            const arr = weightclasses.trimRight().split('\n');
             res.json({classes: [
-                {"Men's": arr.slice(0,arr.length-4)}
-                ,{"Women's": arr.slice(arr.length-4,arr.length)
+                {"Mens": arr.slice(0,arr.length-4)}
+                ,{"Womens": arr.slice(arr.length-4,arr.length)
                 }]
             });
         } else {
@@ -29,7 +29,17 @@ router.get('/', (req, res) => {
 // @desc    Send a request to the ufc website and get a list of top fighters in a weight class given a class
 // @access  Public
 router.get('/:class', (req, res) => {
-    res.json({operation: "Top fighters of " + req.params.class});
+    //res.json({operation: "Top fighters of " + req.params.class});
+    request('https://www.ufc.com/rankings', function (error, response, body) {
+        if((response && response.statusCode) === 200) {
+            const $ = cheerio.load(body);
+            const fighters = $('#block-mainpagecontent div div:nth-child(2) div div div div.view-content').text();
+            console.log(fighters);
+            res.json({});
+        } else {
+            res.json({classes: "Error"});
+        }
+    });
 });
 
 module.exports = router;
