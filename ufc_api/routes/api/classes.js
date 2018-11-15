a0860b8e3b9fa149c14ea9a824680f79994171e8
pkkan <pk.kandru@gmail.com>
2018-11-14 23:58:23 -0500

    Parsing ufc page to get weight classes. Split Men's and Women's divisions

--- ufc_api/routes/api/classes.js
+++ ufc_api/routes/api/classes.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const request = require("request");
+const cheerio = require('cheerio')
+
 const router = express.Router();
 
 // @route   GET api/classes
 // @desc    Send a request to the ufc website and get a list of the current weight classes
 // @access  Public
 router.get('/', (req, res) => {
-    res.json({operation: "List of current weight classes"});
+    //res.json({operation: "List of current weight classes"});
+    request('https://www.jfgjeaofnaofnkf.com/athletes/all?filters%5B0%5D=status%3A23', function (error, response, body) {
+        if((response && response.statusCode) === 200) {
+            const $ = cheerio.load(body);
+            const list = $('#block-mainpagecontent div:nth-child(1) div.block-facet--links:nth-child(2) div.facets-widget-links ul li.facet-item').text();
+            const arr = list.trimRight().split('\n');
+            res.json({classes: [
+                {"Men's": arr.slice(0,arr.length-4)}
+                ,{"Women's": arr.slice(arr.length-4,arr.length)
+                }]
+            });
+        } else {
+            res.json({classes: "Error"});
+        }
+    });
 });
 
 // @route   GET api/classes/class
